70fe945c4fa026f573de338950635ef494d4787b
zghcxq <sungongpcd373@gmail.com>
2019-08-03 11:23:53 -0400

    f

---
+++ js/moretext.js
@@ -0,0 +1,13 @@
+function showdiv(id){
+ 
+if(showdiv_display = document.getElementById(id).style.display=='none'){//如果show是隐藏的
+ 
+document.getElementById(id).style.display='block';//show的display属性设置为block（显示）
+ 
+}else{//如果show是显示的
+ 
+document.getElementById(id).style.display='none';//show的display属性设置为none（隐藏）
+ 
+}
+ 
+}
